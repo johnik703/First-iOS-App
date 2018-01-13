75c42b89b98a50c29d3d95102817ba58203e9c91
Daniel Peach <dpeaches96@gmail.com>
2018-01-13 16:44:45 -0500

    Initial Commit

---
+++ Companies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Companies
+//
+//  Created by Daniel Peach on 1/13/18.
+//  Copyright © 2018 Daniel Peach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
